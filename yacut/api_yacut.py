b045b586452c0f0a08909f5b91fb25a80c6680e8
Иван Едоков <ivanedokov@yandex.ru>
2023-12-15 12:28:13 +0400

    pytest

--- yacut/api_yacut.py
+++ yacut/api_yacut.py
@@ -13,26 +13,29 @@ from .utils import short_url_generator
 def add_urlshort():
     data = request.get_json()
     if not data:
-        raise InvalidAPIUsage('Отсутствует тело запроса', HTTPStatus.BAD_REQUEST)
+        raise InvalidAPIUsage('Отсутствует тело запроса', 400)
     if 'url' not in data:
-        raise InvalidAPIUsage('\"url\" является обязательным полем!', HTTPStatus.BAD_REQUEST)
+        raise InvalidAPIUsage('\"url\" является обязательным полем!', 400)
     if 'short_link' not in data or data['short_link'] == '':
         data['short_link'] = short_url_generator()
     if URLMap.query.filter_by(short=data['short_link']).first() is not None:
-        raise InvalidAPIUsage('Предложенный вариант короткой ссылки уже существует.', HTTPStatus.BAD_REQUEST)
+        raise InvalidAPIUsage('Предложенный вариант короткой ссылки уже существует.', 400)
     if not match(r'^[A-Za-z0-9]+$', data['short_link']) or len(data['short_link']) > 16:
-        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки', HTTPStatus.BAD_REQUEST)
+        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки', 400)
     urlshort = URLMap()
     urlshort.from_dict(data)
     db.session.add(urlshort)
     db.session.commit()
-    return jsonify(urlshort.to_dict()), HTTPStatus.CREATED
+    return jsonify({
+        'url': urlshort.original,
+        'short_link': 'http://localhost/' + urlshort.short
+    }), 201
 
 
 @app.route('/api/id/<string:short_id>/', methods=['GET'])
 def get_urlshort(short_id):
     urlshort = URLMap.query.filter_by(short=short_id).first()
     if not urlshort:
-        raise InvalidAPIUsage('Указанный id не найден', HTTPStatus.NOT_FOUND)
+        raise InvalidAPIUsage('Указанный id не найден', 404)
     data = urlshort.to_dict()
-    return jsonify({'url': data['original']}), HTTPStatus.OK
+    return jsonify({'url': data['original']}), 200
